b65c9f6502b4c8dc36f20908413dc6dd522bb2ce
Dakota Miller <ddmiller1995@gmail.com>
2015-11-12 12:18:25 -0800

    Add password validation

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@
     'use strict';
     var app = angular.module('TeamApp', []);
     app.controller('AppCtrl', ['$scope', function($scope) {
-    	
-    	}]);
-    })();
+
+    	$scope.comparePass = function(password, confirmPassword, form) {
+    		if(password !== confirmPassword) {
+    			console.log(form);
+    			form.confirmP.$setValidity("required", false)
+    		}
+    	}
+
+    }]);
+})();
