a67448c66b05abc3ad592feb0f6902e08289c617
Brandyn Bayes <brandyn.bayes@landspeed.com>
2015-11-16 20:06:17 -0800

    Added Protractor Test Files

---
+++ test/signup-spec.js
@@ -0,0 +1,10 @@
+'use strict';
+var url = 'http://localhost:8080/team-challenge/index.html';
+describe('Birthday Field', function() {
+    beforeEach(function() {
+        browser.get(url);
+    });
+    it('should be able to input a birthdate', function() {
+
+    });
+});
