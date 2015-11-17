faeb8793d1776c462663bf7df0d27ea5471862ab
Brandyn Bayes <brandyn.bayes@landspeed.com>
2015-11-17 13:07:58 -0800

    Added global button variable Fixed browser URL

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,10 +1,13 @@
 'use strict';
-var url = 'http://localhost:8080/team-challenge/index.html';
+var url = 'http://localhost:8000/index.html';
+var btn = element(by.id('signup'));
 describe('Birthday Field', function() {
+    var input = element(by.id('birthday'));
     beforeEach(function() {
         browser.get(url);
     });
-    it('should be able to input a birthdate', function() {
-
+    it('should reject non-dates', function() {
+        input.sendKeys('foobar');
+        expect(btn.isEnabled()).toEqual(false);
     });
 });
